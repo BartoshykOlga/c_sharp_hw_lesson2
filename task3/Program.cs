b346041383b5896207156af7cb8d66fc2533c46e
Bartoshyk Olga <eireen@mail.ru>
2024-02-12 15:42:23 +0300

    task 3

---
+++ task3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход целое число из отрезка [10, 99]
+// и показывает наибольшую цифру числа.
+
+int Number = 79;
+int firstDigit = Number/10;
+int secondDigit  = Number % 10;
+if (firstDigit >= secondDigit)
+{
+    Console.WriteLine ($" наибольшая цифра числа {Number} это {firstDigit}");
+}
+else
+{
+    Console.WriteLine ($" наибольшая цифра числа {Number} это {secondDigit}");
+}
